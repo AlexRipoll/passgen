4e82f33f7ec1b9ada15903de40167b1dc0fcccd5
Alex Ripoll <aripoll@enchainte.com>
2021-02-24 12:20:50 +0100

    Adds custom password generation

--- passgen/passgen.go
+++ passgen/passgen.go
@@ -4,10 +4,12 @@ import (
 	"crypto/rand"
 	"errors"
 	"flag"
-	"fmt"
 	"math/big"
 	"strings"
 )
+
+// TODO REFACTOR: remove duplicated code
+// TODO add goroutines for selecting characters more efficiently
 
 var (
 	schemes = map[string]string{
@@ -16,6 +18,11 @@ var (
 		"base58":      "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789",
 		"base64":      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
 	}
+
+	lower   = "abcdefghijklmnopqrstuvwxyz"
+	upper   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digits  = "0123456789"
+	special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 )
 
 var (
@@ -24,9 +31,12 @@ var (
 )
 
 type Password struct {
-	Scheme  Scheme
-	Length  int
-	Entropy int
+	Scheme       Scheme
+	Length       int
+	Capitals     int
+	Digits       int
+	SpecialChars int
+	Entropy      int
 }
 
 type Scheme struct {
@@ -34,10 +44,17 @@ type Scheme struct {
 	Characters string
 }
 
+type Custom struct {
+	Amount int
+}
+
 func New() (string, error) {
 	var p Password
 	flag.StringVar(&p.Scheme.Name, "s", "base64", "the encoding scheme to use for generating the password")
-	flag.IntVar(&p.Length, "n", 8, "the number of characters to have in the password")
+	flag.IntVar(&p.Length, "l", 8, "the desired password length")
+	flag.IntVar(&p.Capitals, "C", 0, "the desired number of upper-case letters in the password")
+	flag.IntVar(&p.Digits, "N", 0, "the desired number of digits in the password")
+	flag.IntVar(&p.SpecialChars, "S", 0, "the desired number of special characters in the password")
 
 	flag.Parse()
 
@@ -48,17 +65,29 @@ func New() (string, error) {
 	if p.Length <= 0 {
 		return "", ErrInvalidLength
 	}
-	pass, err := generate(p.Scheme, p.Length)
-	if err != nil {
-		return "", err
+
+	var pass string
+	var err error
+	// if none of the specific flags are set then generate a totally random password.
+	if p.Capitals == 0 && p.Digits == 0 && p.SpecialChars == 0 {
+		pass, err = generate(p.Scheme, p.Length)
+		if err != nil {
+			return "", err
+		}
+	}else {
+		pass, err = generateCustom(p.Length, p.Capitals, p.Digits, p.SpecialChars)
+		if err != nil {
+			return "", err
+		}
 	}
+
 	return pass, nil
 }
 
 func (p *Password) schemeValidation() error {
 	schemeName := strings.ToLower(p.Scheme.Name)
 
-	switch  schemeName{
+	switch schemeName {
 	case "hexadecimal", "base32", "base58", "base64":
 		p.Scheme.Characters = schemes[schemeName]
 		return nil
@@ -72,7 +101,6 @@ func generate(scheme Scheme, length int) (string, error) {
 	b := make([]byte, length)
 	_, err := rand.Read(b)
 	if err != nil {
-		fmt.Println("error:", err)
 		return "", err
 	}
 	for i := 0; i < length; i++ {
@@ -85,4 +113,83 @@ func generate(scheme Scheme, length int) (string, error) {
 	}
 
 	return string(b), nil
+}
+
+func generateCustom(length, caps, nums, speChars int) (string, error) {
+
+	requirementLength := caps + nums + speChars
+
+	passLength := max(length, requirementLength)
+	chars := make([]byte, passLength-1)
+
+	if caps > 0 {
+		res, err := selector(upper, caps)
+		if err != nil {
+			return "", err
+		}
+		chars = append(chars, res...)
+	}
+	if nums > 0 {
+		res, err := selector(digits, nums)
+		if err != nil {
+			return "", err
+		}
+		chars = append(chars, res...)
+	}
+	if speChars > 0 {
+		res, err := selector(special, speChars)
+		if err != nil {
+			return "", err
+		}
+		chars = append(chars, res...)
+	}
+	if length > requirementLength {
+		remain := length - requirementLength
+		res, err := selector(lower, remain)
+		if err != nil {
+			return "", err
+		}
+		chars = append(chars, res...)
+	}
+
+	mix := make([]byte, len(chars))
+	it := len(chars) - 1
+	for i := 0; i < it; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		if err != nil {
+			return "", err
+		}
+		mix[i] = chars[num.Int64()]
+		chars[num.Int64()] = chars[len(chars) - 1]
+		chars[len(chars) -1 ] = 0
+		chars = chars[:len(chars) -1]
+	}
+	mix[len(mix) - 1] = chars[0]
+
+	return string(mix), nil
+}
+
+func selector(alphabet string, amount int) ([]byte, error) {
+	b := make([]byte, amount)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < amount; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(alphabet))))
+		if err != nil {
+			return nil, err
+		}
+
+		b[i] = alphabet[num.Int64()]
+	}
+	return b, nil
+}
+
+func max(input1, input2 int) int {
+	if input1 > input2 {
+		return input1
+	}
+	return input2
 }
